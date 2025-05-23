4a7b65164e8956a55c659cc1fac344bb5b3c1572
tudan110 <wtudan@126.com>
2025-05-23 16:06:16 +0800

    feat: TOAST UI Image-Editor for Vue3 demo

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import 'tui-color-picker/dist/tui-color-picker.css';
+import 'tui-image-editor/dist/tui-image-editor.css';
+import TuiImageEditor from '@tudan110/vue3-image-editor';
+import {localeZh, myTheme} from './editor-config'
+
+import {ref} from 'vue';
+
+// 控制是否使用默认 UI
+const useDefaultUI = ref(true);
+
+// 配置选项
+const options = {
+  includeUI: {
+    loadImage: {
+      path: new URL("/src/assets/test.png", import.meta.url).href, //图片地址,
+      name: 'image'
+    },
+    menu: ['resize', 'crop', 'rotate', 'draw', 'shape', 'icon', 'text', 'filter'], // 底部菜单按钮列表 隐藏镜像flip和遮罩mask
+    initMenu: 'draw', // 默认打开的菜单项
+    menuBarPosition: 'bottom', // 菜单所在的位置
+    locale: localeZh, // 本地化语言为中文
+    theme: myTheme // 自定义样式
+  },
+  cssMaxWidth: 700, // canvas 最大宽度
+  cssMaxHeight: 500 // canvas 最大高度
+};
+</script>
+
+<template>
+  <div class="editor">
+    <tui-image-editor
+        :use-default-ui="useDefaultUI"
+        :options="options"
+    />
+  </div>
+</template>
+
+<style scoped>
+.editor {
+  width: 100vh;
+  height: 80vh;
+}
+</style>
---
+++ src/editor-config.js
@@ -0,0 +1,231 @@
+export const localeZh = {
+  Crop: '裁剪',
+  Delete: '删除',
+  DeleteAll: '删除所有',
+  Load: '选择图片',
+  Download: '下载图片',
+  Sharpen: '锐化',
+  Emboss: '浮雕',
+  Blur: '模糊',
+  Sepia: '深褐色',
+  Sepia2: '深褐色2',
+  Invert: '反色',
+  Grayscale: '灰度化',
+  'Remove White': '移除白色',
+  Threshold: '阈值',
+  Distance: '距离',
+  Filter: '滤镜',
+  'Color Filter': '颜色滤镜',
+  Pixelate: '像素化',
+  Noise: '噪点',
+  Brightness: '亮度',
+  Redo: '重做',
+  Undo: '撤销',
+  Reset: '重设',
+  Flip: '翻转',
+  Rotate: '旋转',
+  Draw: '绘画',
+  Shape: '图形',
+  Icon: '图标',
+  Text: '文字',
+  Mask: '遮罩',
+  Apply: '应用',
+  Cancel: '取消',
+  Custom: '自定义',
+  Square: '正方形',
+  'Load Mask Image': '选择遮罩图片',
+  Range: '角度',
+  Color: '颜色',
+  Fill: '填充',
+  Stroke: '边框',
+  Rectangle: '长方形',
+  Circle: '圆形',
+  Triangle: '三角形',
+  Bold: '加粗',
+  Italic: '倾斜',
+  Underline: '下横线',
+  Left: '左对齐',
+  Center: '居中',
+  Right: '右对齐',
+  'Text size': '文本大小',
+  Arrow: '箭头',
+  'Arrow-2': '箭头2',
+  'Arrow-3': '箭头3',
+  Value: '值',
+  'Custom icon': '自定义图标',
+  'Gradient transparency': '渐变透明',
+  Blend: '混合',
+  Multiply: '叠加',
+  Tint: '色调',
+  Heart: '心',
+  Bubble: '泡泡',
+  Location: '位置',
+  Polygon: '多边形',
+  'Star-1': '星星1',
+  'Star-2': '星星2',
+  Free: '自由',
+  Straight: '直线',
+  ZoomIn: '放大',
+  ZoomOut: '缩小',
+  Hand: '拖动',
+  History: '历史',
+  'Flip X': 'X 轴',
+  'Flip Y': 'Y 轴'
+}
+
+// 画布组件自定义样式
+const customTheme = {
+  "common.bi.image": "", // 左上角logo图片
+  "common.bisize.width": "0px",
+  "common.bisize.height": "0px",
+  "common.backgroundImage": "none",
+  "common.backgroundColor": "#f3f4f6",
+  "common.border": "1px solid #333",
+
+  // header
+  "header.backgroundImage": "none",
+  "header.backgroundColor": "#f3f4f6",
+  "header.border": "0px",
+
+  // load button
+  "loadButton.backgroundColor": "#fff",
+  "loadButton.border": "1px solid #ddd",
+  "loadButton.color": "#222",
+  "loadButton.fontFamily": "NotoSans, sans-serif",
+  "loadButton.fontSize": "12px",
+  "loadButton.display": "none", // 可以直接隐藏掉
+
+  // download button
+  "downloadButton.backgroundColor": "#fdba3b",
+  "downloadButton.border": "1px solid #fdba3b",
+  "downloadButton.color": "#fff",
+  "downloadButton.fontFamily": "NotoSans, sans-serif",
+  "downloadButton.fontSize": "12px",
+  "downloadButton.display": "none", // 可以直接隐藏掉
+
+  // icons default
+  "menu.normalIcon.color": "#8a8a8a",
+  "menu.activeIcon.color": "#555555",
+  "menu.disabledIcon.color": "#ccc",
+  "menu.hoverIcon.color": "#e9e9e9",
+  "submenu.normalIcon.color": "#8a8a8a",
+  "submenu.activeIcon.color": "#e9e9e9",
+
+  "menu.iconSize.width": "24px",
+  "menu.iconSize.height": "24px",
+  "submenu.iconSize.width": "32px",
+  "submenu.iconSize.height": "32px",
+
+  // submenu primary color
+  "submenu.backgroundColor": "#1e1e1e",
+  "submenu.partition.color": "#858585",
+
+  // submenu labels
+  "submenu.normalLabel.color": "#858585",
+  "submenu.normalLabel.fontWeight": "lighter",
+  "submenu.activeLabel.color": "#fff",
+  "submenu.activeLabel.fontWeight": "lighter",
+
+  // checkbox style
+  "checkbox.border": "1px solid #ccc",
+  "checkbox.backgroundColor": "#fff",
+
+  // rango style
+  "range.pointer.color": "#fff",
+  "range.bar.color": "#666",
+  "range.subbar.color": "#d1d1d1",
+
+  "range.disabledPointer.color": "#414141",
+  "range.disabledBar.color": "#282828",
+  "range.disabledSubbar.color": "#414141",
+
+  "range.value.color": "#fff",
+  "range.value.fontWeight": "lighter",
+  "range.value.fontSize": "11px",
+  "range.value.border": "1px solid #353535",
+  "range.value.backgroundColor": "#151515",
+  "range.title.color": "#fff",
+  "range.title.fontWeight": "lighter",
+
+  // colorpicker style
+  "colorpicker.button.border": "1px solid #1e1e1e",
+  "colorpicker.title.color": "#fff",
+};
+
+export const myTheme = {
+  'common.bi.image': '',
+  'common.bisize.width': '0',
+  'common.bisize.height': '0',
+  'common.backgroundImage': 'none',
+  'common.backgroundColor': '#ffffff',
+  'common.border': '0px',
+
+  // header
+  'header.backgroundImage': 'none',
+  'header.backgroundColor': 'transparent',
+  'header.border': '0px',
+
+  // load button
+  'loadButton.backgroundColor': '#fff',
+  'loadButton.border': '1px solid #ddd',
+  'loadButton.color': '#222',
+  'loadButton.fontFamily': 'NotoSans, sans-serif',
+  'loadButton.fontSize': '12px',
+  "loadButton.display": "none", // 可以直接隐藏掉
+
+  // download button
+  'downloadButton.backgroundColor': '#3ba26e',
+  'downloadButton.border': '1px solid #3ba26e',
+  'downloadButton.color': '#fff',
+  'downloadButton.fontFamily': 'NotoSans, sans-serif',
+  'downloadButton.fontSize': '12px',
+  "downloadButton.display": "none", // 可以直接隐藏掉
+
+  // icons default
+  'menu.normalIcon.color': '#8a8a8a',
+  'menu.activeIcon.color': '#555555',
+  'menu.disabledIcon.color': '#434343',
+  'menu.hoverIcon.color': '#e9e9e9',
+  'submenu.normalIcon.color': '#e9e9e9',
+  'submenu.activeIcon.color': '#8a8a8a',
+
+  'menu.iconSize.width': '24px',
+  'menu.iconSize.height': '24px',
+  'submenu.iconSize.width': '32px',
+  'submenu.iconSize.height': '32px',
+
+  // submenu primary color
+  'submenu.backgroundColor': '#ffffff',
+  'submenu.partition.color': '#858585',
+
+  // submenu labels
+  'submenu.normalLabel.color': '#000',
+  'submenu.normalLabel.fontWeight': 'bold',
+  'submenu.activeLabel.color': '#000',
+  'submenu.activeLabel.fontWeight': 'bold',
+
+  // checkbox style
+  'checkbox.border': '1px solid #ccc',
+  'checkbox.backgroundColor': '#fff',
+
+  // rango style
+  'range.pointer.color': '#3ba26e',
+  'range.bar.color': '#666',
+  'range.subbar.color': '#d1d1d1',
+
+  'range.disabledPointer.color': '#ddd',
+  'range.disabledBar.color': '#ddd',
+  'range.disabledSubbar.color': '#ddd',
+
+  'range.value.color': '#fff',
+  'range.value.fontWeight': 'lighter',
+  'range.value.fontSize': '11px',
+  'range.value.border': '1px solid #353535',
+  'range.value.backgroundColor': '#151515',
+  'range.title.color': '#fff',
+  'range.title.fontWeight': 'lighter',
+
+  // colorpicker style
+  'colorpicker.button.border': '1px solid #1e1e1e',
+  'colorpicker.title.color': '#fff'
+};
